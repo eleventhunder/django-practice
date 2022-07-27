38fa7e48daa81e90a61869cb563319f57429d606
eleventhunder <digvijaykcs007@gmail.com>
2022-07-27 18:56:56 +0530

    added comments/documentation in polls app

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 import datetime
 
 # Create your models here.
-
+# Model to save Questions
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -14,7 +14,7 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
-
+# Model for saving choices of questions
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
